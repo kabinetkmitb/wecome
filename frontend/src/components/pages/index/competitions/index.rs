5c409fee2877d9975f5a524fb62e5fa396a890f6
MSMazaya <msulthanmazaya@gmail.com>
2022-02-12 12:42:29 +0700

    refactor(frontend): refactor competition component to reduce redundancy

--- frontend/src/components/pages/index/competitions/index.rs
+++ frontend/src/components/pages/index/competitions/index.rs
@@ -1,9 +1,72 @@
 use super::mobile_view::MobileView;
 use yew::prelude::*;
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct LombaKategori {
+	pub icon_src: String,
+	pub name: String,
+}
+
+fn to_lomba_kategori(icon_src: String, name: String) -> LombaKategori {
+	LombaKategori { icon_src, name }
+}
+
 #[function_component(Competitions)]
 pub fn competitions() -> Html {
+	let kategori_lomba = vec![
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644607498/Frame_1_pfwwty.png",
+			),
+			String::from("Riset"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641100/Color_Pallete_m7klnt.png",
+			),
+			String::from("Seni"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641102/Group_80_r4hjdj.png",
+			),
+			String::from("Akademik"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641355/Group_87_zu03ow.png",
+			),
+			String::from("Teknologi"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641358/Frame_2_qfhdmq.png",
+			),
+			String::from("Bisnis"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641363/Frame_3_zzsb2w.png",
+			),
+			String::from("Bahasa"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641365/Group_119_fxnn0x.png",
+			),
+			String::from("Olahraga"),
+		),
+		to_lomba_kategori(
+			String::from(
+				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641368/image_11_auruqz.png",
+			),
+			String::from("Konferensi"),
+		),
+	];
+
+	let kategori_lomba_length = kategori_lomba.len();
+
 	html! {
-		<MobileView/>
+		<MobileView {kategori_lomba} {kategori_lomba_length}/>
 	}
 }
--- frontend/src/components/pages/index/competitions/mobile_view.rs
+++ frontend/src/components/pages/index/competitions/mobile_view.rs
@@ -1,78 +1,27 @@
+use super::index::LombaKategori;
 use yew::prelude::*;
-
-pub struct LombaKategori {
-	icon_src: String,
-	name: String,
-}
+use yew::{function_component, html, Properties};
 
 struct PaginationIndex {
 	upper_bound: usize,
 	lower_bound: usize,
 }
 
-fn to_lomba_kategori(icon_src: String, name: String) -> LombaKategori {
-	LombaKategori { icon_src, name }
+#[derive(Properties, PartialEq)]
+pub struct Props {
+	pub kategori_lomba: Vec<LombaKategori>,
+	pub kategori_lomba_length: usize,
 }
 
 #[function_component(MobileView)]
-pub fn mobile_view() -> Html {
+pub fn mobile_view(props: &Props) -> Html {
 	let pagination = use_state(|| PaginationIndex {
 		upper_bound: 1,
 		lower_bound: 0,
 	});
 
-	let kategori_lomba = vec![
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644607498/Frame_1_pfwwty.png",
-			),
-			String::from("Riset"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641100/Color_Pallete_m7klnt.png",
-			),
-			String::from("Seni"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641102/Group_80_r4hjdj.png",
-			),
-			String::from("Akademik"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641355/Group_87_zu03ow.png",
-			),
-			String::from("Teknologi"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641358/Frame_2_qfhdmq.png",
-			),
-			String::from("Bisnis"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641363/Frame_3_zzsb2w.png",
-			),
-			String::from("Bahasa"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641365/Group_119_fxnn0x.png",
-			),
-			String::from("Olahraga"),
-		),
-		to_lomba_kategori(
-			String::from(
-				"https://res.cloudinary.com/dw4bwn79m/image/upload/v1644641368/image_11_auruqz.png",
-			),
-			String::from("Konferensi"),
-		),
-	];
-
-	let kategori_length = kategori_lomba.len();
+	let kategori_lomba = props.kategori_lomba.clone();
+	let kategori_lomba_length = props.kategori_lomba_length.clone();
 
 	let left_click = {
 		let pagination = pagination.clone();
@@ -81,13 +30,13 @@ pub fn mobile_view() -> Html {
 
 		Callback::from(move |_| {
 			pagination.set(PaginationIndex {
-				upper_bound: if upper_bound as i32 + 2 > kategori_length as i32 - 1 {
-					upper_bound + 2 - kategori_length
+				upper_bound: if upper_bound as i32 + 2 > kategori_lomba_length as i32 - 1 {
+					upper_bound + 2 - kategori_lomba_length
 				} else {
 					upper_bound + 2
 				},
-				lower_bound: if lower_bound as i32 + 2 > kategori_length as i32 - 1 {
-					lower_bound + 2 - kategori_length
+				lower_bound: if lower_bound as i32 + 2 > kategori_lomba_length as i32 - 1 {
+					lower_bound + 2 - kategori_lomba_length
 				} else {
 					lower_bound + 2
 				},
@@ -103,12 +52,12 @@ pub fn mobile_view() -> Html {
 		Callback::from(move |_| {
 			pagination.set(PaginationIndex {
 				upper_bound: if upper_bound as i32 - 2 < 0 {
-					kategori_length + upper_bound - 2
+					kategori_lomba_length + upper_bound - 2
 				} else {
 					upper_bound - 2
 				},
 				lower_bound: if lower_bound as i32 - 2 < 0 {
-					kategori_length + lower_bound - 2
+					kategori_lomba_length + lower_bound - 2
 				} else {
 					lower_bound - 2
 				},
